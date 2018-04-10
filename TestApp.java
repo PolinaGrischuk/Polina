df2e20bb38d5b25f3f9e6333ccf18267e1c5a1f2
Polina Grischuk <polinka0524@gmail.com>
2018-04-10 15:22:17 +0300

    L1Commit2

---
+++ TestApp.java
@@ -0,0 +1,15 @@
+package testapp;
+public class TestApp {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+// TODO code application logic here
+Scanner in = new Scanner(System.in);
+System.out.print("1st num: ");
+double a = Double.valueOf(in.nextLine());
+System.out.print("2nd num: ");
+double b = Double.valueOf(in.nextLine());
+System.out.println("a+b="+(a+b));
+System.out.println("GIT test application");
+}}
