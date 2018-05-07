1547d92c0a3ee3e4812504a01d16469f534dc5d2
Polina Grischuk <polinka0524@gmail.com>
2018-05-07 22:59:42 +0300

    lab2commit6

--- TestApp.java
+++ TestApp.java
@@ -5,13 +5,22 @@ public class TestApp
 /**
 * @param args the command line arguments
 */
-public static void main(String[] args) {
-// TODO code application logic here
-Scanner in = new Scanner(System.in);
-System.out.print("1st num: ");
-double a = Double.valueOf(in.nextLine());
-System.out.print("2nd num: ");
-double b = Double.valueOf(in.nextLine());
-System.out.println("a+b="+(a+b));
-System.out.println("GIT test application");
-}}
+package testapp;
+public class SimpleCalc {
+
+private double a;
+private double b;
+
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
+}
+
+public double getSum() {
+return this.a+this.b;
+}
+
+}
+public double getvidnimanna() {
+return this.a-this.b;
+}
--- newClass1.java
+++ newClass1.java
@@ -11,3 +11,17 @@
 public class newClass1 {
     
 }
+package testapp;
+public class SimpleCalc {
+
+private double a;
+private double b;
+
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
+}
+sdfgadfbhadb
+}
+
+}
