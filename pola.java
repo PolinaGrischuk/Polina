ab05015b0d182441edd4c95f17b25f2aca42d569
Polina Grischuk <polinka0524@gmail.com>
2018-05-08 18:09:52 +0300

    l2commit23

--- pola.java
+++ pola.java
@@ -8,13 +8,11 @@ SimpleCalc(double p1,double p2) {
 this.a=p1;
 this.b=p2;
 }
-System.out.println("a*b="+(sc.getMult()));
 public double getSum() {
 return this.a+this.b;
 }
 public double getSum() {
 return this.a+this.b;
 }
-System.out.println("a*b="+(sc.getMult()));
 
 }
