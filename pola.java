d536cbb1f0141e2b3030d2d58a1a1d77e6bcb6ab
Polina Grischuk <polinka0524@gmail.com>
2018-05-08 14:48:15 +0300

    l2commit12

--- pola.java
+++ pola.java
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author polinagrischuk
- */
-public class pola {
-    
-}
 package testapp;
 public class SimpleCalc {
 
@@ -21,14 +8,8 @@ SimpleCalc(double p1,double p2) {
 this.a=p1;
 this.b=p2;
 }
-Kgfjhdrs 
-Iklykyhhmj 
-dhtfykghk.lkuyjrtfjg
 
 public double getSum() {
-fgdfhhsjttt
-
-Лабораторна робота No2. Технології програмування
 return this.a+this.b;
 }
 
